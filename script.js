74306c520ffd22881741e9c35ae4d6f7b66ec1b3
Chris Cudworth <chris.cudworth@outlook.com>
2015-06-07 13:34:21 -0400

    Updated stylesheet.css

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 $(document).ready(function(){
-runProg(10);
-});
 
-var runProg = function(num){
+
+
+
+var num = 10;
 
 for (var i = 0; i < num*num; i++) {
     $('#wrapper').append('<div class="block"></div>');   
 }
+
     var sideValue = Math.floor(500/num);
     var side = sideValue + "px";
     $(".block").css({'height':side, 'width':side});
-$('.block').hover(function() {
+$(".block").hover(function() {
         $(this).addClass('selected');
 });
-};
+
 
 $('#reset').click(function() {
-    $('.block').animate({opacity: 0});
-    $('.block').removeClass('selected');
-    $('.block').animate({opacity: 1});
+    $(".block").animate({opacity: 0});
+    $(".block").removeClass('selected');
+    $(".block").animate({opacity: 1});
        
     }); 
 $('#size').click(function() {  
-    var newSize = prompt('Enter a new size');
-    $('.block').remove();
-    runProg(newSize);
+    var newSize = prompt('Enter a new size: 10, 25, 50 or 100');
+    $(".block").remove();
+    for (var i = 0; i < newSize*newSize; i++) {
+        $('#wrapper').append('<div class="block"></div>');   
+}
+    var sideValue = Math.floor(500/newSize);
+    var side = sideValue + "px";
+    $(".block").css({'height':side, 'width':side});
+    $(".block").hover(function() {
+        $(this).addClass('selected');
+});
     
     
     });
 
+});
