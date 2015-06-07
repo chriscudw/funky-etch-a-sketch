26570c6ed19acd73bb6c29321f8b7dc3132d676e
Chris Cudworth <chris.cudworth@outlook.com>
2015-06-07 12:29:06 -0400

    Second commit

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 $(document).ready(function(){
+runProg(10);
+});
 
-var size = 20;
-for (var i = 0; i < size*size; i++) {
+var runProg = function(num){
+
+for (var i = 0; i < num*num; i++) {
     $('#wrapper').append('<div class="block"></div>');   
 }
+    var sideValue = Math.floor(500/num);
+    var side = sideValue + "px";
+    $(".block").css({'height':side, 'width':side});
 $('.block').hover(function() {
         $(this).addClass('selected');
 });
-
+};
 
 $('#reset').click(function() {
     $('.block').animate({opacity: 0});
     $('.block').removeClass('selected');
     $('.block').animate({opacity: 1});
-    $(this).animate({opacity: 0.5});
-    $(this).animate({opacity: 1});
        
     }); 
 $('#size').click(function() {  
-    var newSize = prompt('Enter a new height');
+    var newSize = prompt('Enter a new size');
     $('.block').remove();
-    for (var i = 0; i < newSize*newSize; i++) {
-    $('#wrapper').append('<div class="block"></div>');   
-}
-    $('.block').hover(function() {
-        $(this).addClass('selected');
-});
+    runProg(newSize);
     
     
     });
-    
-});
+
